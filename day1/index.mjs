9224118af021a4ed13cfaa57373d006139f2a26c
Ryan Breen <ryan@ryanbreen.com>
2020-12-16 22:27:54 -0500

    Day 1 in the books.

--- day1/index.mjs
+++ day1/index.mjs
@@ -12,34 +12,32 @@ export const part1 = async () => {
 
     const input = await(promises.readFile(new URL(`${sourcePath}.csv`, import.meta.url), 'utf-8'))
 
-    let answer;
-
     // Sort, then walk outside in lopping off the end of the array where sum at idx + end is
     // greater than 2020.
     let workingSet = _.map(_.split(input, '\n'), i => { return parseInt(i)} )
     workingSet.sort((a, b) => { return a-b })
 
-    for (let i = 0; i < workingSet.length; i += 1) {
-        let backOfArray = workingSet.length - 1
+    const calculator = () => {
 
-        while (workingSet[i] + workingSet[backOfArray] > 2020) {
-            workingSet.pop()
-            backOfArray -= 1
-        }
-
-        if (workingSet[i] + workingSet[backOfArray] == 2020) {
-            answer = workingSet[i] * workingSet[backOfArray]
-            break
-        } else {
-            // Once we're below 2020, move to the next candidate
-            continue
+        for (let i = 0; i < workingSet.length; i += 1) {
+            for (let j = workingSet.length - 1; j > i; j -= 1) {
+                if (workingSet[i] + workingSet[j] === 2020) {
+                    return workingSet[i] * workingSet[j]
+                }
+            }
         }
     }
 
-    const key = parseInt(await(promises.readFile(new URL(`${sourcePath}.1.key`, import.meta.url), 'utf-8')))
-    assert(key === answer)
+    const answer = calculator()
 
-    console.log("day 1, part 1 ✅")
+    process.stdout.write(`day 1, part 1: ${answer} `)
+
+    const key = parseInt(await(promises.readFile(new URL(`${sourcePath}.1.key`, import.meta.url), 'utf-8')))
+    if (key === answer) {
+        console.log("✅")
+    } else {
+        console.log("❌")
+    } 
 }
 
 export const part2 = async () => {
@@ -59,7 +57,6 @@ export const part2 = async () => {
             for (let j = i + 1; j < workingSet.length; j += 1) {
                 for (let k = workingSet.length - 1; k > j; k -= 1) {
                     if (workingSet[i] + workingSet[j] + workingSet[k] === 2020) {
-                        console.log(`${i}==${workingSet[i]} ${j}==${workingSet[j]} ${k}==${workingSet[k]}`)
                         return workingSet[i] * workingSet[j] * workingSet[k]
                     }
                 }
